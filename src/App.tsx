6a08e93562fb432c4f9f5a38d96c542bfe280418
001Rakib <rakibrakibrakib354@gmail.com>
2024-07-12 20:14:19 +0600

    added page refresh warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import { useEffect } from "react";
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
+  const { cartProducts } = useAppSelector((state) => state.cart);
+
+  //for giving page refresh warning
+  useEffect(() => {
+    if (!cartProducts.length) return;
+
+    const handlePageReload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      return e.returnValue("");
+    };
+
+    window.addEventListener("beforeunload", handlePageReload, {
+      capture: true,
+    });
+    return () => {
+      window.removeEventListener("beforeunload", handlePageReload, {
+        capture: true,
+      });
+    };
+  }, [cartProducts.length]);
+
   return (
     <div>
       <Navbar></Navbar>
